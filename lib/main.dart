b4858a16e5fb3f3b0d823619cf9a17c757d1a9bc
Iwaide <iwaide@hoobby.net>
2019-12-30 10:10:47 +0900

    Add category route

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,11 @@
 import 'package:flutter/material.dart';
-import './category.dart';
+import './category_route.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.green[100],
-        appBar: AppBar(
-          title: Text('Hello Rectangle'),
-        ),
-        body: Category(
-          name: 'Settings',
-          color: Colors.blueAccent,
-          iconLocation: Icons.settings,
-        ),
-      ),
+      home: CategoryRoute()
     ),
   );
 }
